e39b471e151235fa2791d56c8e80e92c9cfae5f7
ymralin <48262970+ymralin@users.noreply.github.com>
2023-10-31 23:02:07 +0100

    Implementing lane traffic.

--- car_lane.py
+++ car_lane.py
@@ -9,12 +9,23 @@ class Lane:
         self.lane_active = True
         self.directions = [-1, 1]
         self.direction = self.directions[random.randint(0, 1)]
+        self.lane_activity = 40
+        self.car_check = 0
 
     def create_car(self):
         new_car = Car()
-        new_car.goto(320 * -self.direction, 0)
+        new_car.goto(320 * -self.direction, self.y_cor)
         self.cars.append(new_car)
 
     def move_cars(self):
         for _ in self.cars:
             _.goto(_.xcor() + self.direction * _.car_speed, _.ycor())
+            if (self.direction == -1 and _.xcor() < -320) or (self.direction == 1 and _.xcor() > 320):
+                self.cars.pop(0)
+                _.reset()
+                _.ht()
+
+    def generate_cars(self):
+        self.car_check = random.randint(0, 100)
+        if self.car_check < self.lane_activity:
+            self.create_car()
--- main.py
+++ main.py
@@ -22,6 +22,8 @@ def start_game():
 
     game_is_on = True
     game_tick = 0.1
+    tick_count = 0
+    lanes = []
 
     screen.onkeypress(player.set_movement_up, "w")
     screen.onkeyrelease(player.reset_movement, "w")
@@ -32,7 +34,11 @@ def start_game():
     screen.onkeypress(player.set_movement_right, "d")
     screen.onkeyrelease(player.reset_movement, "d")
 
-    new_lane = Lane()
+    # range(0, 3) for testing. add variable number of lanes
+    for _ in range(0, 3):
+        new_lane = Lane()
+        new_lane.y_cor = 0 + _ * 60
+        lanes.append(new_lane)
 
     def add_car():
         new_lane.create_car()
@@ -41,9 +47,13 @@ def start_game():
 
     while game_is_on:
         player.player_move()
-        new_lane.move_cars()
+        for _ in range(0, len(lanes)):
+            if (tick_count + _) % 10 == 0:
+                lanes[_].generate_cars()
+            lanes[_].move_cars()
         time.sleep(game_tick)
         screen.update()
+        tick_count += 1
 
 
 screen.onkey(start_game, "Return")
